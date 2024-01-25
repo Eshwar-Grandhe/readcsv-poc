452eff5b616d0646632556379afc29fdb7c6a4f4
Eshwar Grandhe <anirudhanish1@gmail.com>
2024-01-25 17:05:23 -0600

    added readcsv component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ShowCSV from './components/readcsv'
+
+function App() {
+  return (
+    <ShowCSV />
+  );
+}
+
+export default App;
---
+++ src/components/readcsv.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export default function ShowCSV() {
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+    </div>
+  );
+
+}
+
