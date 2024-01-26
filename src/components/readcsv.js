8f182fac72268643202f1bf29d52f6b6c6b1b828
Eshwar Grandhe <anirudhanish1@gmail.com>
2024-01-25 23:54:43 -0600

    added mock csv file

--- src/components/readcsv.js
+++ src/components/readcsv.js
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Papa from 'papaparse'
+import csvfile from './data/data.csv'
 
 export default function ShowCSV() {
+    // const csvData = useState();
+    function loadCSV(){
+        console.log("Hi from loadCSV function");
+        
+        // let csv = Papa.parse(csvfile, {Headers:true});
 
+    }
   return (
     <div>
-      <h1>Counters that update together</h1>
+      <h1>Read CSV Poc</h1>
+      <input type="button" value="Click to display the csv" onClick={loadCSV} />
+
     </div>
   );
 
